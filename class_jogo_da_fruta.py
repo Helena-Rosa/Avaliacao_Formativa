ef467968aeafc63f49332150a525c14457f70657
Helena Rosa <helena.rosa@aluno.senai.br>
2025-11-14 15:02:44 -0300

    ADD: falta fazer o banco de dados da avaliação formativa

--- class_jogo_da_fruta.py
+++ class_jogo_da_fruta.py
@@ -2,6 +2,9 @@ import ttkbootstrap as ttk
 import tkinter as tk
 import sqlite3
 from tkinter import messagebox
+import random
+
+lista_frutas=['🍉','🍊','🍒','🍓','🍎']
 
 
 class SorteioDeFrutas:
@@ -14,49 +17,69 @@ class SorteioDeFrutas:
         self.janela.geometry("950x950+0+0")
 
        
-       
         
-        label_caixa1 = ttk.Label (self.janela)
-                                        
-        label_caixa1.pack()
-
-
-        label_caixa2 = ttk.Label (self.janela)
-                                        
-        label_caixa2.pack()
-
-
-        label_caixa3 = ttk.Label (self.janela)
-                                        
-        label_caixa3.pack()
-
-
-
-        # #campo de espaço para escrever as infomaçoes pedidas (item_nome) 
-        # self.campo_item = ttk.Entry(self.janela, width=10)
-        # self.campo_item.pack(pady=5)
-
-
-
-        
-        label_sorteio = ttk.Label (self.janela,
+        self.label_sorteio = ttk.Label (self.janela,
                                         text= "SORTEIO DE FRUTAS",
                                         font=("Broadway" , 25))
-        label_sorteio.pack()
+        self.label_sorteio.pack()
 
 
-        label_fruta1 = ttk.Label (self.janela,
+        self.label_fruta1 = ttk.Label (self.janela,
                                   text="🌺",
                                   font=("Arial", 40)
                                   ,relief="raised")
-        label_fruta1.pack()
+        self.label_fruta1.pack()
 
 
 
-        # #informações do cabeçalho
-        # self.treeview["columns"] = ( "ID", "ITEM", "DESCRIÇÃO", "STATUS" )
-        # self.treeview["show"] = "headings"
+        self.label_fruta2 = ttk.Label (self.janela,
+                                  text="🌺",
+                                  font=("Arial", 40)
+                                  ,relief="raised")
+        self.label_fruta2.pack()
 
+
+        self.label_fruta3 = ttk.Label (self.janela,
+                                  text="🌺",
+                                  font=("Arial", 40)
+                                  ,relief="raised")
+        self.label_fruta3.pack()
+
+
+
+        frame_botao = ttk.Frame(self.janela)
+        frame_botao.pack () 
+
+
+        ttk.Button(frame_botao, text="SORTEAR",command=self.soreteio).pack(side="right", pady= 100, padx=70 )
+
+
+        label_jogadas = ttk.Label (self.janela,
+                                        text= "Historico de Jogadas",
+                                        font=("Arial" , 15))
+        label_jogadas.pack()
+
+
+
+        self.treeview = ttk.Treeview(self.janela)
+        self.treeview.pack(padx=15, pady=20, fill="both", expand=True)
+
+
+
+
+    def soreteio(self):
+         fruta1= random.choice(lista_frutas)
+         fruta2= random.choice(lista_frutas)
+         fruta3= random.choice(lista_frutas)
+
+
+         self.label_fruta1.config(text=fruta1)
+         self.label_fruta2.config(text=fruta2)
+         self.label_fruta3.config(text=fruta3)
+         
+
+
+       
     def run(self):
             self.janela.mainloop()
 
